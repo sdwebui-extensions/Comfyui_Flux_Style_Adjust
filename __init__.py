5bfb9884dcaf705d5dc46382470e75be097f8829
tanglup <160642431+tanglup@users.noreply.github.com>
2024-12-03 03:37:15 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from .nodes import StyleModelAdvancedApply
 
 NODE_CLASS_MAPPINGS = {
-    "StyleModelAdvancedApply": StyleModelAdvancedApply
+    "StyleModelAdvancedApply": StyleModelAdvancedApply,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "StyleModelAdvancedApply": "Style Model Apply (Advanced)"
-} 
+    "StyleModelAdvancedApply": "Style Model Advanced Apply",
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS'] 
